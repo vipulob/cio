8130814e3c6459aa553bd55baccd5131a6015f19
Vipul Borikar <vipucell@gmail.com>
2018-01-28 14:42:39 +0530

    Issue: Main program needs some data or parameter they say.
    Solution:
    Accepting arguments from main function.

--- cio.c
+++ cio.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "cio.h"
 
-int main()
+int main(int argc, char *argv[])
 {
 	printf("Starting CIO program \n");
+
+	printf("The Arguments passed are: %d \n", argc);
 
 	initialize_fio();
 
